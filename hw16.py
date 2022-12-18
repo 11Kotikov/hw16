fa2e8baef55ba15054de1e5b2d8abb5db78c76c7
Kotikov <11deadsluts@gmail.com>
2022-12-18 22:30:44 +0300

    third task is solved

--- hw16.py
+++ hw16.py
@@ -37,4 +37,40 @@
 #         for z in 0,1:
 #             print(f'x = {x}, y = {y}, z = {z} ->',not (x or y or z) == (not x and not y and not z))
 
+# Напишите программу, которая принимает на вход координаты точки (X и Y), 
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+# в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+def checking_quaters (x,y):
+    if (x>0) & (y>0):
+        print ('It\'s in the first quater')
+    elif (x<0) & (y>0):
+        print ('It\'s in the second quater') 
+    elif (x<0) & (y<0):
+        print ('It\'s in the third quater')
+    elif (x>0) & (y<0):
+        print ('It\'s in the fouth quater')
+    elif (x==0) & (y<0):
+        print ('It\'s located on the axis Y between the third and fouth quater')
+    elif (x>0) & (y==0):
+        print ('It\'s located on the axis X between the second and third quater')
+    elif (x==0) & (y>0):
+        print ('It\'s located on the axis Y between the first and second quater')
+    elif (x<0) & (y==0):
+        print ('It\'s located on the axis X between the first and third quater')
+    elif (x==0) & (y==0):
+        print ('Sorry, it can't be in ZERO. Bu if you wonder - it\'s in the center of the coordinate system')
+    else:
+        print ('wow')
+
+print ('Hello, this program let you check the quaters for X and Y values in 2D dimension ')
+print ('Pls, input integer or float values for X:')
+x = float(input())
+print ('Pls, input integer or float values for Y:')
+y = float(input())
+checking_quaters(x,y)
 
