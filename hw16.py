f55231d380bea2ea263243f1957c607ef243ee49
Kotikov <11deadsluts@gmail.com>
2022-12-18 19:47:01 +0300

    first task is solved

---
+++ hw16.py
@@ -0,0 +1,34 @@
+# Задача 1. Напишите программу, которая принимает на вход цифру, 
+# обозначающую день недели, и проверяет, является ли 
+# этот день выходным.
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+# def checking_weekends (user_input):
+#     user_input = int (user_input)
+#     if type(user_input) == int:
+#         while (user_input < 1) or (user_input > 7):
+#                 print ('incorrect input, please try again')
+#                 user_input = int(input())
+#         else:
+#             if (user_input == 6) or (user_input == 7) :
+#                 print ('YES! It\'s the weekend, congratulations! No work today')
+#             elif (user_input > 0) and (user_input < 6):
+#                 print ('Sorry, this is weekday, you better get back to work')
+#     elif type(user_input) == str:
+#         print ('Sorry, but you\'re not a winner, run the program again')
+#     else:
+#         print ('Sorry, but you\'re not a winner, run the program again')
+
+
+# print ('Input the number of the day in week: ')
+# day_week = input()
+# if (day_week.isdigit()==True):
+#     checking_weekends(day_week)
+# else:
+#     print ('Sorry, but you\'re not a winner, run the program again')
+
+
+
